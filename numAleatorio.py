7c453838805c8a124d83dfb8cef31b7f0121fd40
ahruiz <ahruiz0@gmail.com>
2025-06-16 14:59:22 -0700

    se incluye try/except y control de entrada

--- numAleatorio.py
+++ numAleatorio.py
@@ -6,12 +6,28 @@ print("GENERAMOS UN NUMERO OCULTO ALEATORIO ENTRE EL 1 Y EL 10") #explicacion de
 
 numEscondido = random.randint(1, 10) #se genera un num aleatorio oculto
 
-try:  #manejo de errores........
-    numero = int(input("escribe el numero que piensas que se generó: "))  #pedimos al usuario que adivine el num
+# try:  #manejo de errores........
+#     numero = int(input("Escribe el numero que piensas que se generó: "))  #pedimos al usuario que adivine el num
 
-    if numero != numEscondido:    #comparamos la entrada con el numero oculto y damos la sentencia
-        print("Mala suerte!!! Intentalo de nuevo!!!")
-    else:
-        print(f"FELICIDADES.....ganaste un cupon!!!!!")
-except:
-    print("Los datos capturados no son correctos....") # damos la sentencia de datos incorrectos
+#     if numero != numEscondido:    #comparamos la entrada con el numero oculto y damos la sentencia
+#         print("Mala suerte!!! Intentalo de nuevo!!!")
+#     else:
+#         print(f"FELICIDADES.....ganaste un cupon!!!!!")
+# except:
+#     print("Los datos capturados no son correctos....") # damos la sentencia de datos incorrectos
+
+numero = input("Escribe el numero que piensas que se genero: ") #pedimos al usr ingresar el num
+
+if not numero.isdigit():  #comprobamos que sea digito
+    print("Los datos ingresados son incorrectos......") # damos la sentencia si no l es y salimos del prog
+    exit()
+
+numero = int(numero)  #convertimos la variable a entero
+if numero < 1 or numero > 10: #comprobamos que este entre 1 y 10
+    print("Debes capturar numeros entre 1 y 10.....") # damos la sentencia si no lo esta y salimos del prog
+    exit()
+
+if numero != numEscondido:    # comprobamos que la entrada sea diferente del num aleatorio oculto
+    print("¡¡¡MALA SUERTE.....Intentalo de nuevo!!!") #damos la sentencia de fallo a ese intento
+else:
+    print("¡¡¡FELICIDADES!!!....Has ganado un CUPON ") # damos la sentencia de acertado
