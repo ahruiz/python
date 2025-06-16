fd7819c9957a8bb1dfb72ca2989a999a3d695a4f
ahruiz <ahruiz0@gmail.com>
2025-06-16 11:36:13 -0700

    SE AGREGAN COMENTARIOS.....

--- mostrarysumar.py
+++ mostrarysumar.py
@@ -1,9 +1,13 @@
 #Codigo para pedir 2 numeros, sumarlos y mostrarlos
 
 
-num1 = int(input("ingresa el primer numero: "))
-num2 = int(input("ingresa el segundo numero: "))
-                                                     
-totSuma = num1 + num2
+num1 = input("ingresa el primer numero: ")
+num2 = input("ingresa el segundo numero: ") #pedimos al usr ingresar los datos
 
-print(f"Los numeros capturados son {num1} y {num2} y la suma de ambos es {totSuma}")
+if not num1.isdigit() or not num2.isdigit(): #verificamos q la entradas sean enteros
+    print("Los valores capturados no son correctos.....") # advertimos el error y cerramos 
+    exit()  
+  
+totSuma = num1 + num2 #efectuamos la operacion
+
+print(f"Los numeros capturados son {num1} y {num2} y la suma de ambos es {totSuma}") #mostramos los datos
