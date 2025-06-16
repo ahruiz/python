f211ab8948de1df20c6a7b543ee7f478b95e86f7
ahruiz <ahruiz0@gmail.com>
2025-06-16 15:08:29 -0700

    se agregan comentarios.....

--- numMayor.py
+++ numMayor.py
@@ -1,11 +1,11 @@
 #codigo para identificar el numero mayor de una lista
 
-print("identificar el numerpo mas alto de una lista dada")
+print("identificar el numerpo mas alto de una lista dada") #explicacion
 
-lista = [-10,-1,0,-12,-13,-5,-17,-33,-87,-99]
+misNums = [-10,-1,0,-12,-13,-5,-17,-33,-87,-99]   #definimos los nums de la lista
 
-nummay = max(misNums)
+nummay = max(misNums) #sacamos el num mayor 
 
-print(f"Mi lista de numeros es: {misNums}")
-print(f"El numero mayor en mi lista es : {nummay}")
+print(f"Mi lista de numeros es: {misNums}") #mostramos los numeros de la lista a comparar
+print(f"El numero mayor en mi lista es : {nummay}") #mostramos el mayor
 
